6440cda03a8c3e1230c74b1cf41cd71b0cae72d9
Joey Kuo <joeyskuo@gmail.com>
2023-07-12 22:26:32 -0700

    wrapper useEffect init setUiData to DataStore; fix ProductSection name

--- src/views/ProductSectionWrapper.js
+++ src/views/ProductSectionWrapper.js
@@ -1,1 +1,18 @@
-import react from "react";
+import React, { useEffect, useState } from "react";
+import ProductSection from './ProductSection';
+
+const ProductSectionWrapper = props => {
+
+    const [uiData, setUiData] = useState(null);
+
+    useEffect(() => {
+        
+        if(props.registerUseStateHook) registerUseStateHook(setUiData);
+
+    }, props.registerUseStateHook);
+
+
+    return (
+        <ProductSection {...props} />
+    )
+}
