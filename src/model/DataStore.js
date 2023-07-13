886134815eab394b244d2f8f97e8d80fcbb77669
Joey Kuo <joeyskuo@gmail.com>
2023-07-12 22:09:23 -0700

    added DataStore methods

--- src/model/DataStore.js
+++ src/model/DataStore.js
@@ -1,6 +1,21 @@
 
 class DataStore {
+
     constructor() {
         this._data = {};
+        this._updateDataFunction = null;
     }
+
+    getData() {
+        return this._data;
+    }
+
+    setData(newData) {
+        if(this._updateDataFunction) this._updateDataFunction(newData);
+    }
+
+    setUpdateDataFunction(updateDataFunction) {
+        this._updateDataFunction = updateDataFunction;
+    }
+
 }
