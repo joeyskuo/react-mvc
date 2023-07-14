051c79159af20d1a0942920c1b78406303da764a
Joey Kuo <joeyskuo@gmail.com>
2023-07-13 20:21:27 -0700

    created new folder structure

---
+++ src/ProductFeature/product-feature.js
@@ -0,0 +1,18 @@
+import controllerFunction from "./controller/main-controller";
+
+const init = () => {
+
+    // get root component
+
+    // get initial data
+
+    // transform data
+
+    // set transformed data to DataStore
+
+    // render wrapper component
+
+    return;
+}
+
+export default init;
