85a11e420946fd2bcba09a69e40fe17023bbb430
Joey Kuo <joeyskuo@gmail.com>
2023-07-13 21:15:41 -0700

    add handling of initialData for wrapper component

--- src/ProductFeature/views/ProductSectionWrapper.js
+++ src/ProductFeature/views/ProductSectionWrapper.js
@@ -5,15 +5,20 @@ const ProductSectionWrapper = props => {
 
     const [uiData, setUiData] = useState(null);
 
+    const {controllerFunction, initialData } = props;
+
     useEffect(() => {
         
         props.registerUpdateStateFunction?.(setUiData);
 
     }, props.registerUpdateStateFunction);
 
+    const data = uiData ?? initialData;
 
     return (
-        <ProductSection {...props} />
+        <ProductSection 
+            controllerFunction={controllerFunction}
+            uiData={data} />
     )
 }
 
