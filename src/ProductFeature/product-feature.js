a19654ab3bf577f93fdf15ffabbabeb77472962f
Joey Kuo <joeyskuo@gmail.com>
2023-07-13 21:02:37 -0700

    add methods for initializing root component

--- src/ProductFeature/product-feature.js
+++ src/ProductFeature/product-feature.js
@@ -1,16 +1,23 @@
+import { createRoot } from 'react-dom/client';
 import controllerFunction from "./controller/main-controller";
+import ProductSectionWrapper from './views/ProductSectionWrapper';
 
 const init = () => {
 
     // get root component
+    const domNode = document.getElementById('#product-section');
+    if(!domNode) return;
 
-    // get initial data
-
-    // transform data
+    // get initial data and transform it to work well with our components
+    const initialRawData = {};
+    const transformedData = transformer.transform(initialRawData);
 
     // set transformed data to DataStore
+    
 
     // render wrapper component
+    const root = createRoot(domNode);
+    root.render(<ProductSectionWrapper controllerFunction={controllerFunction} />);
 
     return;
 }
